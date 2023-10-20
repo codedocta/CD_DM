a4e700efd61a57e43cd6c6e98a08a89cbd325308
Nick <codedocta@gmail.com>
2023-10-19 20:56:34 -0500

    changed version numbers == for dependencies in setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from Directory_Manager import version
 
 setup(
     name='CD_MD',
-    version= version.__version__,
+    version=version.__version__,
     description='A package containing utilities for file and directory management, ZIP operations, and more.',
     author='Nicholas Capasso',
     author_email='codedocta@gmail.com',
     url='https://github.com/codedocta/CD_MD',
     packages=find_packages(),
     install_requires=[
-        'pyside6=6.5.3',  # For FileDialog with PySide6
-        'pyqt6=6.5.2',  # For FileDialog with PyQt6
+        'pyside6==6.5.3',  # For FileDialog with PySide6
+        'pyqt6==6.5.2',  # For FileDialog with PyQt6
     ],
     classifiers=[
         'Development Status :: 2 - Beta',
